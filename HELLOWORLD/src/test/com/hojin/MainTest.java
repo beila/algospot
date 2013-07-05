8bcda64e9d4c9658c46592509ffc36ff03060545
Hojin Ghim <hojinni@gmail.com>
2013-07-06 08:27:55 +0900

    HELLOWORLD 뼈대

---
+++ HELLOWORLD/src/test/com/hojin/MainTest.java
@@ -0,0 +1,45 @@
+package test.com.hojin; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Main Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>7¿ù 6, 2013</pre> 
+* @version 1.0 
+*/ 
+public class MainTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: mainOne(String arg) 
+* 
+*/ 
+@Test
+public void testMainOne() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
