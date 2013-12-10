4f3e7696dd10fc34a5e766d6fd6b9791e7c7950f
Hojin Ghim <beila@kaist.ac.kr>
2013-12-11 08:00:37 +0900

    test Main.main

--- HELLOWORLD/src/com/hojin/Main.java
+++ HELLOWORLD/src/com/hojin/Main.java
@@ -2,19 +2,23 @@ package com.hojin;
 
 import java.io.IOException;
 
+/**
+ * http://algospot.com/judge/problem/read/HELLOWORLD
+ */
 public class Main {
 
     public static void main(String[] args) throws IOException {
         byte[] input = new byte[10];
+        //noinspection ResultOfMethodCallIgnored
         System.in.read(input);
         final int round = parseInt(input);
         for (int i = 0; i < round; ++i) {
+            //noinspection ResultOfMethodCallIgnored
             System.in.read(input);
             System.out.println(new Main().mainOne(new String(input)));
         }
     }
 
-    //TODO
     private static int parseInt(byte[] input) {
         return Integer.valueOf(new String(input));
     }
--- HELLOWORLD/src/test/com/hojin/MainTest.java
+++ HELLOWORLD/src/test/com/hojin/MainTest.java
@@ -5,6 +5,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import static junit.framework.Assert.assertEquals;
 
 /** 
@@ -17,14 +22,20 @@ import static junit.framework.Assert.assertEquals;
 public class MainTest {
 
     private Main main;
+    private InputStream systemInputStream;
+    private PrintStream systemOutputStream;
 
     @Before
 public void before() throws Exception {
-    main = new Main();
+        systemInputStream = System.in;
+        systemOutputStream = System.out;
+        main = new Main();
 } 
 
 @After
-public void after() throws Exception { 
+public void after() throws Exception {
+    System.setIn(systemInputStream);
+    System.setOut(systemOutputStream);
 } 
 
 /** 
@@ -33,9 +44,13 @@ public void after() throws Exception {
 * 
 */ 
 @Test
-public void testMain() throws Exception { 
-//TODO: Test goes here... 
-} 
+public void testMain() throws Exception {
+    System.setIn(new ByteArrayInputStream("5\n1\n2\n3\n4\n5\n".getBytes()));
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+    Main.main(new String[]{});
+    //TODO assert out has 5 lines
+}
 
 /** 
 * 
