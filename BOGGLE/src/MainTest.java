602fe7a3af8dfdb83c807fcf91d0e6f9b8534918
Hojin Ghim <hojin.ghim@lge.com>
2014-03-09 16:30:53 +0900

    test BOGGLE extreme

--- BOGGLE/src/MainTest.java
+++ BOGGLE/src/MainTest.java
@@ -2,6 +2,8 @@ import com.beila.testlib.Lib;
 import com.beila.testlib.StringArrayInputOutput;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
 public class MainTest {
@@ -13,6 +15,26 @@ public class MainTest {
             "XTNZY",
             "XOQRS"
     );
+
+    @SuppressWarnings("SpellCheckingInspection")
+    private final String[] FIRST_WORDS = new String[] {
+            "PRETTY",
+            "GIRL",
+            "REPEAT",
+            "KARA",
+            "PANDORA",
+            "GIAZAPX"
+    };
+
+    @SuppressWarnings("SpellCheckingInspection")
+    private final String[] FIRST_OUTPUT = new String[] {
+            "PRETTY YES",
+            "GIRL YES",
+            "REPEAT YES",
+            "KARA NO",
+            "PANDORA NO",
+            "GIAZAPX YES"
+    };
 
     @Test
     public void testFindOneCharacter() throws Exception {
@@ -56,49 +78,128 @@ public class MainTest {
         assertEquals(true, main.findString("PRETTY"));
     }
 
-    @SuppressWarnings("SpellCheckingInspection")
-    static String WORDS = Lib.getLinedString(
-            "PRETTY",
-            "GIRL",
-            "REPEAT",
-            "KARA",
-            "PANDORA",
-            "GIAZAPX"
-    );
-
-    static String USER_INPUT = Lib.getLinedString(
-            "1"
-    ) + FIRST_BOARD + Lib.getLinedString(
-            "6"
-    ) + WORDS;
-
-    @SuppressWarnings("SpellCheckingInspection")
-    static String[] EXPECTED_OUTPUT = new String[] {
-            "PRETTY YES",
-            "GIRL YES",
-            "REPEAT YES",
-            "KARA NO",
-            "PANDORA NO",
-            "GIAZAPX YES"
-    };
-
     @Test
     public void testFirstExample() throws Exception {
-        try (StringArrayInputOutput io = new StringArrayInputOutput(USER_INPUT)) {
-            Main.main(new String[]{}, io.getInputStream(), io.getPrintStream());
-            Lib.assertDeepEquals(EXPECTED_OUTPUT, io.toStringArray());
-        }
+
+        String USER_INPUT = Lib.getLinedString(
+                "1"
+        ) + FIRST_BOARD + Lib.getLinedString(
+                "6"
+        ) + Lib.getLinedString(
+                FIRST_WORDS
+        );
+
+        assertMain(new String[]{USER_INPUT}, FIRST_OUTPUT);
     }
 
     @Test
     public void testSimpleSample() throws Exception {
         //noinspection SpellCheckingInspection
-        try (StringArrayInputOutput io = new StringArrayInputOutput(Lib.getLinedString(
-                "1", "NNNNS", "NEEEN", "NEYEN", "NEEEN", "NNNNN", "2", "YES", "NO"
-        ))) {
-            Main.main(new String[]{}, io.getInputStream(), io.getPrintStream());
-            Lib.assertDeepEquals(new String[] {"YES YES", "NO NO"}, io.toStringArray());
-        }
+        assertMain(new String[]{
+                "1",
+                "NNNNS",
+                "NEEEN",
+                "NEYEN",
+                "NEEEN",
+                "NNNNN",
+                "2",
+                "YES",
+                "NO"
+        }, new String[] {"YES YES", "NO NO"});
     }
 
+    @Test
+    public void testSimpleSample2() throws Exception {
+        //noinspection SpellCheckingInspection
+        assertMain(new String[]{
+                "1",
+                "NNNNN",
+                "NEEEN",
+                "NEYEN",
+                "NEEEN",
+                "NSNNN",
+                "2",
+                "YES",
+                "NO"
+        }, new String[] {"YES YES", "NO NO"});
+    }
+
+    @Test
+    public void testManyBoards() throws Exception {
+        final int reps = 100_000;
+        StringBuilder USER_INPUT = new StringBuilder();
+        USER_INPUT.append(Lib.getLinedString(String.valueOf(reps)));
+        for (int i = 0; i < reps; i++) {
+            USER_INPUT.append(FIRST_BOARD);
+        }
+        USER_INPUT.append(Lib.getLinedString(String.valueOf(FIRST_WORDS.length)));
+        USER_INPUT.append(Lib.getLinedString(FIRST_WORDS));
+
+        String[] OUTPUT = new String[FIRST_OUTPUT.length* reps];
+        for (int i = 0; i < reps; i++) {
+            System.arraycopy(FIRST_OUTPUT, 0, OUTPUT,
+                    i * FIRST_OUTPUT.length, FIRST_OUTPUT.length);
+        }
+
+        assertMain(new String[]{USER_INPUT.toString()}, OUTPUT);
+    }
+
+    @Test
+    public void testManyWords() throws Exception {
+        final int reps = 100_000;
+        StringBuilder input = new StringBuilder();
+        input.append(Lib.getLinedString("1"));
+        input.append(FIRST_BOARD);
+        input.append(Lib.getLinedString(String.valueOf(reps*FIRST_WORDS.length)));
+        for (int i = 0; i < reps; i++) {
+            input.append(Lib.getLinedString(FIRST_WORDS));
+        }
+
+        String[] output = new String[reps*FIRST_OUTPUT.length];
+        for (int i = 0; i < reps; i++) {
+            System.arraycopy(FIRST_OUTPUT, 0, output,
+                    i * FIRST_OUTPUT.length, FIRST_OUTPUT.length);
+        }
+
+        assertMain(new String[]{input.toString()}, output);
+    }
+
+    @Test
+    public void testNothingFound() throws Exception {
+        @SuppressWarnings("SpellCheckingInspection")
+        String[] input = new String[] {"1\n" +
+                "AAAAA\n" +
+                "AAAAA\n" +
+                "AAAAA\n" +
+                "AAAAA\n" +
+                "AAAAA\n" +
+                "1\n" +
+                "AAAAAAAAAB"};
+        @SuppressWarnings("SpellCheckingInspection")
+        String[] output = new String[] {"AAAAAAAAAB" + " NO"};
+        assertMain(input, output);
+    }
+
+    @Test
+    public void testNothingFound2() throws Exception {
+        @SuppressWarnings("SpellCheckingInspection")
+        String[] input = new String[] {"1\n" +
+                "AAAAA\n" +
+                "AAAAA\n" +
+                "AAAAA\n" +
+                "AAACC\n" +
+                "AAACB\n" +
+                "1\n" +
+                "AAAAAAAAAB"};
+        @SuppressWarnings("SpellCheckingInspection")
+        String[] output = new String[] {"AAAAAAAAAB" + " NO"};
+        assertMain(input, output);
+    }
+
+    public static void assertMain(String[] input, String[] output) throws IOException {
+        try (StringArrayInputOutput io = new StringArrayInputOutput(input)) {
+            Main.main(new String[]{}, io.getInputStream(), io.getPrintStream());
+            Lib.assertDeepEquals(output, io.toStringArray());
+        }
+    }
 }
