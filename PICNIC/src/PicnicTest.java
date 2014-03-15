f7d4d0c36f313adb738d32d56df8257f84ca659f
Hojin Ghim <hojin.ghim@lge.com>
2014-03-15 18:22:28 +0900

    PICNIC  testTenFriendsOutOfSixStudents

--- PICNIC/src/PicnicTest.java
+++ PICNIC/src/PicnicTest.java
@@ -10,6 +10,8 @@ import org.junit.rules.Timeout;
 public class PicnicTest {
     @Rule
     public Timeout globalTimeout = new Timeout(1000);
+    private static final boolean t = true;
+    private static final boolean f = false;
 
     @Test
     public void testAPairOfTwoFriends() throws Exception {
@@ -38,6 +40,29 @@ public class PicnicTest {
                 {true, true, true, false},
         });
         Assert.assertEquals(3, main.countPairingCases());
+    }
+
+    @Test
+    public void testTenFriendsOutOfSixStudents() throws Exception {
+        // 0 1
+        // 0 2
+        // 1 2
+        // 1 3
+        // 1 4
+        // 2 3
+        // 2 4
+        // 3 4
+        // 3 5
+        // 4 5
+        Main main = new Main(new boolean[][]{
+                {f, t, t, f, f, f}, //0
+                {t, f, t, t, t, f}, //1
+                {t, t, f, t, t, f}, //2
+                {f, t, t, f, t, t}, //3
+                {f, t, t, t, f, t}, //4
+                {f, f, f, t, t, f}, //5
+        });
+        Assert.assertEquals(4, main.countPairingCases());
     }
 
 }
