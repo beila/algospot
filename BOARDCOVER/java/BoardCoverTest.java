e804496a0e6b915b9e386b3d4a15aaf15d47aa7b
Hojin Ghim <hojin.ghim@lge.com>
2014-03-15 19:45:13 +0900

    BoardCoverTest.testInit()

---
+++ BOARDCOVER/java/BoardCoverTest.java
@@ -0,0 +1,20 @@
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+/**
+ * http://algospot.com/judge/problem/read/BOARDCOVER
+ * Created by hojin.ghim on 3/15/14.
+ */
+public class BoardCoverTest {
+    @Rule
+    public Timeout globalTimeout = new Timeout(2000);
+
+    static final private boolean t = true;
+    static final private boolean f = false;
+
+    @Test
+    public void testInit() throws Exception {
+        new Main(new boolean[][]{{f, t}, {t, t}});
+    }
+}
---
+++ BOARDCOVER/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * http://algospot.com/judge/problem/read/BOARDCOVER
+ * Created by hojin.ghim on 3/15/14.
+ */
+public class Main {
+    private final boolean[][] board;
+
+    public Main(boolean[][] board) {
+        this.board = board;
+    }
+}
