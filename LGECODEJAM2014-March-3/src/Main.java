f0e519edfc1e24142f459d0e9bff177a4c5eba11
Hojin Ghim <hojin.ghim@lge.com>
2014-03-29 16:39:00 +0900

    TelegramTest.testInit

---
+++ LGECODEJAM2014-March-3/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * http://www.lgecodejam.com/codejam/dashboard
+ * http://www.lgecodejam.com/codejam/problem/download/pdf/5/kr
+ * Created by hojin.ghim on 3/29/14.
+ */
+public class Main {
+    public Main(String[] strings) {
+    }
+}
---
+++ LGECODEJAM2014-March-3/src/TelegramTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * http://www.lgecodejam.com/codejam/dashboard
+ * http://www.lgecodejam.com/codejam/problem/download/pdf/5/kr
+ * Created by hojin.ghim on 3/29/14.
+ */
+public class TelegramTest {
+    @Test
+    public void testInit() throws Exception {
+        new Main(new String[]{"zebra", "bras", "ebbs"});
+    }
+}
