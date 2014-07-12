9f9becce55ea4266c9e7d49aa025d03d384752b1
Hojin Ghim <beila@kaist.ac.kr>
2014-07-12 18:26:03 +0900

    Dynamic Programing first example

--- DYNAMICPROGRAMMING/src/DynamicProgrammingTest.java
+++ DYNAMICPROGRAMMING/src/DynamicProgrammingTest.java
@@ -1,5 +1,37 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class DynamicProgrammingTest {
+
+    @Test
+    public void testRecursiveBino() throws Exception {
+        assertEquals(1, Main.bino(1, 1));
+    }
+
+    @Test
+    public void testRecursiveBino2_1() throws Exception {
+        assertEquals(2, Main.bino(2, 1));
+    }
+
+    @Test
+    public void testRecursiveBino3_1() throws Exception {
+        assertEquals(3, Main.bino(3, 1));
+    }
+
+    @Test
+    public void testRecursiveBino5_2() throws Exception {
+        assertEquals(10, Main.bino(5, 2));
+    }
+
+    @Test
+    public void testRecursiveBino5_3() throws Exception {
+        assertEquals(10, Main.bino(5, 3));
+    }
+
+    @Test
+    public void testRecursiveBino6_3() throws Exception {
+        assertEquals(20, Main.bino(6, 3));
+    }
 
 }
--- DYNAMICPROGRAMMING/src/Main.java
+++ DYNAMICPROGRAMMING/src/Main.java
@@ -1,5 +1,13 @@
 /**
+ * Chapter 08 Dynamic Programming
  * Created by hojin.ghim on 2014-07-12.
  */
 public class Main {
+    public static int bino(int n, int r) {
+//        if (r == 0 || r == n) return 1;
+//        if (n == 1) return 1;
+        if (r == 1) return n;
+        if (n == r) return 1;
+        return bino(n - 1, r - 1) + bino(n - 1, r);
+    }
 }
