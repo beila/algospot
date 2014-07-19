474328ce32f9add2559a84d09b5b03ec1ea796cf
Hojin Ghim <beila@kaist.ac.kr>
2014-07-19 14:35:29 +0900

    Dynamic Programming: memoization example

--- DYNAMICPROGRAMMING/src/DynamicProgrammingTest.java
+++ DYNAMICPROGRAMMING/src/DynamicProgrammingTest.java
@@ -1,37 +1,55 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DynamicProgrammingTest {
+    private Main binomial;
+
+    @Before
+    public void setUp() throws Exception {
+        binomial = new Main.RecursiveBinomial();
+//        binomial = new Main.MemoizedBinomial();
+    }
 
     @Test
     public void testRecursiveBino() throws Exception {
-        assertEquals(1, Main.bino(1, 1));
+        assertEquals(1, binomial.bino(1, 1));
     }
 
     @Test
     public void testRecursiveBino2_1() throws Exception {
-        assertEquals(2, Main.bino(2, 1));
+        assertEquals(2, binomial.bino(2, 1));
     }
 
     @Test
     public void testRecursiveBino3_1() throws Exception {
-        assertEquals(3, Main.bino(3, 1));
+        assertEquals(3, binomial.bino(3, 1));
     }
 
     @Test
     public void testRecursiveBino5_2() throws Exception {
-        assertEquals(10, Main.bino(5, 2));
+        assertEquals(10, binomial.bino(5, 2));
     }
 
     @Test
     public void testRecursiveBino5_3() throws Exception {
-        assertEquals(10, Main.bino(5, 3));
+        assertEquals(10, binomial.bino(5, 3));
     }
 
     @Test
     public void testRecursiveBino6_3() throws Exception {
-        assertEquals(20, Main.bino(6, 3));
+        assertEquals(20, binomial.bino(6, 3));
+    }
+
+    @Test
+    public void testRecursiveBino29_15() throws Exception {
+        assertEquals(77558760, new Main.RecursiveBinomial().bino(29, 15));
+    }
+
+    @Test
+    public void testMemoizedBino29_15() throws Exception {
+        assertEquals(77558760, new Main.MemoizedBinomial().bino(29, 15));
     }
 
 }
--- DYNAMICPROGRAMMING/src/Main.java
+++ DYNAMICPROGRAMMING/src/Main.java
@@ -2,12 +2,58 @@
  * Chapter 08 Dynamic Programming
  * Created by hojin.ghim on 2014-07-12.
  */
-public class Main {
-    public static int bino(int n, int r) {
+public interface Main {
+    int bino(int n, int r);
+
+    static class RecursiveBinomial implements Main {
+        @Override
+        public int bino(int n, int r) {
 //        if (r == 0 || r == n) return 1;
 //        if (n == 1) return 1;
-        if (r == 1) return n;
-        if (n == r) return 1;
-        return bino(n - 1, r - 1) + bino(n - 1, r);
+            if (r == 1) return n;
+            if (n == r) return 1;
+            return bino(n - 1, r - 1) + bino(n - 1, r);
+        }
+    }
+
+    static class MemoizedBinomial implements Main {
+        @Override
+        public int bino(int n, int r) {
+            if (isCached(n, r)) {
+                return getCache(n, r);
+            }
+            int result = bino(n - 1, r - 1) + bino(n - 1, r);
+            setCache(n, r, result);
+            return result;
+        }
+
+        int[][] cache = new int[30][30]; {
+            initCache();
+        }
+
+        private void initCache() {
+            for (int n = 0; n < cache.length; ++n)
+                for (int r = 0; r < cache[n].length; ++r)
+                    cache[n][r] = -1;
+            for (int n = 0; n < cache.length; ++n) {
+                int r = 1;
+                cache[n][r] = n;
+                r = n;
+                cache[n][r] = 1;
+            }
+        }
+
+        private int getCache(int n, int r) {
+            return cache[n][r];
+        }
+
+        private boolean isCached(int n, int r) {
+            return 0 <= cache[n][r];
+        }
+
+        private void setCache(int n, int r, int value) {
+            cache[n][r] = value;
+        }
+
     }
 }
