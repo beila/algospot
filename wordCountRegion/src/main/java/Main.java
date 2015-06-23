874d05d63372edc75d0a8aed20f2a2234e7f1110
Hojin Ghim <hojin.ghim@lge.com>
2015-06-23 22:31:29 +0900

    LGE code jam Word Count Region with small set problems

--- wordCountRegion/src/main/java/Main.java
+++ wordCountRegion/src/main/java/Main.java
@@ -1,19 +1,28 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
+    private int[] mWords;
+    private int[] mPositions;
+    private int[] mTargetFrequencies;
+
     public int solve(int[] words, int[] positions, int[] targetFrequencies) {
         int[] frequencies = new int[targetFrequencies.length];
         int countValid = 0;
-        int minRegion = positions[positions.length - 1];
+        int minRegion = -1;
         int start = 0, end = 0;
         int targetCount = Arrays.stream(targetFrequencies).sum();
 
         if (targetCount == 0) return 0;
 
         int word;
+        int currentRegion;
         while (end < words.length) {
             do {
-                if (end >= words.length) return -1;
+                if (end >= words.length) {
+                    return minRegion;
+                }
                 word = words[end];
                 ++frequencies[word];
                 if (frequencies[word] <= targetFrequencies[word]) ++countValid;
@@ -23,12 +32,31 @@ public class Main {
             while (countValid >= targetCount) {
                 word = words[start];
                 --frequencies[word];
-                if (frequencies[word] <= targetFrequencies[word]) --countValid;
+                if (frequencies[word] < targetFrequencies[word]) --countValid;
                 ++start;
             }
-            int currentRegion = positions[end - 1] - positions[start - 1] + 1;
-            if (currentRegion < minRegion) minRegion = currentRegion;
+            currentRegion = positions[end - 1] - positions[start - 1] + 1;
+            if (minRegion < 0 || currentRegion < minRegion) minRegion = currentRegion;
         }
         return minRegion;
+    }
+
+    public void inputOneCase(Scanner scanner) {
+        int length = scanner.nextInt();
+        mWords = new int[length];
+        mPositions = new int[length];
+        int wordCount = scanner.nextInt();
+        mTargetFrequencies = new int[wordCount + 1];
+        for (int i = 0; i < wordCount; i++) {
+            mTargetFrequencies[i + 1] = scanner.nextInt();
+        }
+        for (int i = 0; i < length; i++) {
+            mWords[i] = scanner.nextInt();
+            mPositions[i] = scanner.nextInt();
+        }
+    }
+
+    public int solve() {
+        return solve(mWords, mPositions, mTargetFrequencies);
     }
 }
--- wordCountRegion/src/test/java/WordCountRegionTest.java
+++ wordCountRegion/src/test/java/WordCountRegionTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 
 public class WordCountRegionTest {
@@ -13,11 +16,47 @@ public class WordCountRegionTest {
 
     @Test
     public void testFirstExample() throws Exception {
-        assertEquals(6, main.solve(new int[]{1, 2, 1, 1, 1, 3, 1, 1, 3, 2}, new int[]{3, 5, 6, 7, 8, 10, 18, 19, 25, 30}, new int[]{0, 2,1,1}));
+        assertEquals(6, main.solve(new int[]{1, 2, 1, 1, 1, 3, 1, 1, 3, 2}, new int[]{3, 5, 6, 7, 8, 10, 18, 19, 25, 30}, new int[]{0, 2, 1, 1}));
     }
 
     @Test
     public void testSecondExample() throws Exception {
         assertEquals(-1, main.solve(new int[]{1, 2, 2}, new int[]{1, 3, 5}, new int[]{0, 2, 1}));
+    }
+
+    @Test
+    public void testFirstSmallInput() throws Exception {
+        Scanner problemScanner = new Scanner(new File("wordCountRegion/data/p1-s01.in"));
+        Scanner solutionScanner = new Scanner(new File("wordCountRegion/data/p1-s01.out"));
+        int cases = problemScanner.nextInt();
+
+        while (--cases > 0) {
+            main.inputOneCase(problemScanner);
+            assertEquals(solutionScanner.nextInt(), main.solve());
+        }
+    }
+
+    @Test
+    public void testSecondSmallInput() throws Exception {
+        Scanner problemScanner = new Scanner(new File("wordCountRegion/data/p1-s02.in"));
+        Scanner solutionScanner = new Scanner(new File("wordCountRegion/data/p1-s02.out"));
+        int cases = problemScanner.nextInt();
+
+        while (--cases > 0) {
+            main.inputOneCase(problemScanner);
+            assertEquals(solutionScanner.nextInt(), main.solve());
+        }
+    }
+
+    @Test
+    public void testThirdSmallInput() throws Exception {
+        Scanner problemScanner = new Scanner(new File("wordCountRegion/data/p1-s03.in"));
+        Scanner solutionScanner = new Scanner(new File("wordCountRegion/data/p1-s03.out"));
+        int cases = problemScanner.nextInt();
+
+        while (--cases > 0) {
+            main.inputOneCase(problemScanner);
+            assertEquals(solutionScanner.nextInt(), main.solve());
+        }
     }
 }
